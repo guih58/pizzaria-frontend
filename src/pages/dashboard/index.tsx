38339ea33ecd22400e8cb7968098859c5f90bb7a
Matheus Guilherme Araujo <matheus.guih58@gmail.com>
2023-02-19 16:33:25 -0300

    Criando o header

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,20 @@
 import { canSSRauth } from "../../utils/canSSRAuth"
+import Head from "next/head"
+import { Header } from "../../components/ui/Header"
+
 
 export default function Dashboard(){
   return(
-    <div>
-      <h1>Bem vindo ao painel</h1>
-    </div>
+    <>
+    <Head>
+      <title>Painel - pizzaria</title>
+    </Head>
+        <div>
+          <Header />
+          <h1>Painel</h1>
+        </div>
+
+    </>
   )
 }
 
